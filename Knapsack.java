dda44f979663239d99d58abe8fe8afa6a22fac21
rafaelscappini <scappinirafael@gmail.com>
2016-12-11 09:31:34 -0200

    Update Knapsack.java

--- Knapsack.java
+++ Knapsack.java
@@ -1,1 +1,14 @@
+package entity;
 
+import java.util.ArrayList;
+
+public class Knapsack() {
+        // Private ArrayList<> variable that keeps Knapsack's items
+        private ArrayList<Item> items;
+        
+        // Private int variable that keeps Knapsack's value
+        private int value;
+        
+        // Private int variable that keeps Knapsack's weight
+        private int weight;
+}
