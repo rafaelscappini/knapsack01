7a671f12fb6042fc9ce159578a9c04b95f145f98
rafaelscappini <scappinirafael@gmail.com>
2016-12-11 09:12:56 -0200

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,9 @@
+package entity;
+
+public class Item() {
+      // Private integer variable that keeps Item's value
+      private int value;
+      
+      // Private integer variable that keeps Item's weight
+      private int weight
+}
