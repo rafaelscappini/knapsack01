91cfb381ed2662b5d148df38910e45cbab2807c1
rafaelscappini <scappinirafael@gmail.com>
2016-12-12 11:29:32 -0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,77 @@
+package entity;
 
+import java.util.Random;
+
+public class Main() {
+        // Create population
+        public static void createPopulation() {
+                System.out.println("Creating population of " + Algorithm.getPopulationSize() + " knapsacks..."); 
+                Population population = new Population(Algorithm.getPopulationSize());
+                System.out.println("Population created successfully!\n");
+        }
+        
+        // Evaluate population
+        public static void evaluatePopulation() {
+                System.out.println("Evaluating population...");
+                population.evaluate();
+                System.out.println("Population evaluated successfully!\n");
+        }
+        
+        // Main
+        public static void main(String[] args) {
+                Algorithm.setCromossomeSize(6);
+                Algorithm.setMaxWeight(30);
+                Algorithm.setMutationRate(50);
+                Algorithm.setPopulationSize(10);
+                
+                createPopulation();
+        }
+        
+        // Roulette Selection method
+        public static Knapsack[] rouletteSelection(Population population) {
+                Knapsack[] selected = new Knapsack[2];
+                population.evaluate();
+                int T = population.getValue();
+                Random r = new Random();
+                int S = 0;
+                for (int i = 0; i < 2; i++) {
+                        int R = r.nextInt(T);
+                        for (int j = 0; j < population.getSize(); j++) {
+                                S += population.getKnapsack(j).getValue();
+                                if (S >= R) {
+                                        selected[j] = population.getKnapsack(j);
+                                }
+                        }
+                }
+                return selected;
+        }
+        
+        // Tour selection method
+        public static Knapsack[] tourSelection(Population population) {
+                Knapsack[] selected = new Knapsack[2];
+                double D = 0.75;
+                Random r = new Random();
+                for (int i = 0; i < population.getSize(); i++) {
+                        Knapsack k1 = population.getKnapsack(r.nextInt(population.getSize()));
+                        Knapsack k2 = population.getKnapsack(r.nextInt(population.getSize()));
+                        while (k1.equals(k2)) {
+                                k2 = population.getKnapsack(r.nextInt(population.getSize()));
+                        }
+                        int R = r.nextInt(2);
+                        if (R < D) {
+                                if (k1.getValue() > k2.getValue()) {
+                                        selected[i] = k1;
+                                } else {
+                                        selected[i] = k2;
+                                }
+                        } else {
+                                if (k1.getValue() > k2.getValue()) {
+                                        selected[i] = k2;
+                                } else {
+                                        selected[i] = k1;
+                                }
+                        }
+                }
+                return selected;
+        }
+}
